ec6ce86b111f6bb27b187c966d99aa5af46671d8
Érico Andrei <ericof@gmail.com>
2016-04-05 14:55:38 +0200

    Plone 5.0.3 does not order handlers the same way

--- collective/newrelic/patches/transformchains.py
+++ collective/newrelic/patches/transformchains.py
@@ -1,23 +1,24 @@
-from plone.transformchain.transformer import (
-                                                ConflictError,\
-                                                getAdapters,\
-                                                ITransform,\
-                                                LOGGER,\
-                                                sort_key,\
-                                                Transformer,\
-                                                DISABLE_TRANSFORM_REQUEST_KEY,
-                                                )
+# -*- coding: utf-8 -*-
+"""Patch plone.transformchain."""
+from collective.newrelic.utils import logger
+from operator import attrgetter
+from plone.transformchain.transformer import ConflictError
+from plone.transformchain.transformer import DISABLE_TRANSFORM_REQUEST_KEY
+from plone.transformchain.transformer import getAdapters
+from plone.transformchain.transformer import ITransform
+from plone.transformchain.transformer import LOGGER
+from plone.transformchain.transformer import Transformer
 from ZServer.FTPRequest import FTPRequest
 
 import newrelic.agent
-from collective.newrelic.utils import logger
 
-#Save original for further use.
+
+#  Save original for further use.
 original_transform_call = Transformer.__call__
 
 
 def newrelic_transform__call__(self, request, result, encoding):
-
+    """Patched transform call."""
     # Don't transform FTP requests
     if isinstance(request, FTPRequest):
         return None
@@ -29,14 +30,15 @@ def newrelic_transform__call__(self, request, result, encoding):
     try:
         published = request.get('PUBLISHED', None)
 
-        handlers = [v[1] for v in getAdapters((published, request,), ITransform)]
-        handlers.sort(sort_key)
+        handlers = sorted(
+            [v[1] for v in getAdapters((published, request,), ITransform)],
+            key=attrgetter('order')
+        )
 
         trans = newrelic.agent.current_transaction()
 
         for handler in handlers:
             with newrelic.agent.FunctionTrace(trans, handler.__class__.__name__, 'Zope/Transform'):
-
                 if isinstance(result, unicode):
                     newResult = handler.transformUnicode(result, encoding)
                 elif isinstance(result, str):
@@ -50,8 +52,12 @@ def newrelic_transform__call__(self, request, result, encoding):
         return result
     except ConflictError:
         raise
-    except Exception, e:
-        LOGGER.exception(u"Unexpected error whilst trying to apply transform chain")
+    except Exception:
+        LOGGER.exception(
+            u'Unexpected error whilst trying to apply transform chain'
+        )
 
 Transformer.__call__ = newrelic_transform__call__
-logger.info("Patched plone.transformchain.transformer:Transformer.__call__ with instrumentation")
+logger.info(
+    'Patched plone.transformchain.transformer:Transformer.__call__ with instrumentation'
+)
